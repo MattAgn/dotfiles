35210330cda0e0d727b5eebb03d0ec0d6b8b93f7
MattAgn <matthieua@bam.tech>
2020-03-03 08:11:45 +0100

    :sparkles: (node) use fnm instead of nvm

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -29,13 +29,12 @@ source ~/.dotfiles/zsh/aliases.zsh
 
 
 ##### NVM ######
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 source ~/.bashrc
 
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
 
+### NFM 
+eval "$(fnm env --multi)"
 
 
 ##### GOOGLE CLOUD & KUBERNETES CONFIG ######
