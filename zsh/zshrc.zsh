3f04463d02be3ea4e74e772040a6e1f4a0772a95
MattAgn <matthieua@bam.tech>
2020-12-04 09:50:48 +0100

    :wrench: add nvm

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -54,3 +54,8 @@ function kubectl() { echo "+ kubectl $@">&2; command kubectl $@; }
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+# NVM 
+ export NVM_DIR="$HOME/.nvm"
+    [ -s "$(brew --prefix)/opt/nvm/nvm.sh" ] && . "$(brew --prefix)/opt/nvm/nvm.sh" # This loads nvm
+    [ -s "$(brew --prefix)/opt/nvm/etc/bash_completion.d/nvm" ] && . "$(brew --prefix)/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion
