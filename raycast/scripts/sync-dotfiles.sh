2d9ab5a8c5c8c2dd5995fc37de0cfeac582df0db
MattAgn <matthieua@bam.tech>
2023-02-06 08:32:42 +0100

    update dotfiles

---
+++ raycast/scripts/sync-dotfiles.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title update dotfiles
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.author Matthieu
+
+cd ~/dotfiles
+git pull
+
